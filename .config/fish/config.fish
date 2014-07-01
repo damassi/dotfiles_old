2a50d0062e41002a486e1fadaa4339debd79fa6c
Christopher Pappas <damassi.pappas@gmail.com>
2014-06-30 23:49:01 -0700

    File additions

---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Theme
+set fish_theme idan
+
+# Plugins
+set fish_plugins git rails rvm sublime node brew bundler
+
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+alias dt='cd ~/Desktop'
+alias Sites='cd ~/Sites'
+alias gh='cd ~/Sites/Github'
+alias cn='cd ~/Sites/CN'
+alias sim='cd ~/Desktop/simulator; and rake'
+
+# Mongo DB
+alias startmongo="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist"
+alias stopmongo="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist"
+
+
